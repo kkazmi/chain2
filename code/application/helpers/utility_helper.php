6eb8cd041d6a7ca240f90c9f1d60ca7f71309810
kkazmi <kkazmi@github.com>
2018-01-19 12:03:49 +0530

    updated wallet

---
+++ code/application/helpers/utility_helper.php
@@ -0,0 +1,42 @@
+
+<?php
+function asset_url()
+{
+   return base_url().'assets/';
+}
+function logo_url()
+{
+	return base_url().'assets/images/tycho-logo.png';
+}
+
+function favicon_url()
+{
+	return base_url().'assets/images/tychofavicon.png';
+}
+function login_bg()
+{
+	return base_url().'assets/images/loginbg.jpg';
+}
+
+function project_name()
+{
+	return 'TYCHO Wallet';
+}
+
+function wallet_mail()
+{
+	return 'support@wallet.io';
+}
+
+function sending_mail()
+{
+	return 'prateekvermaphp@gmail.com';
+}
+
+function sending_mail_pass()
+{
+	return '@prateek123';
+}
+
+
+?>
